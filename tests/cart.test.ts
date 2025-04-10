ee3e8320ea9ccabf66901b10cef57bb9b1f593b1
glafay <guillaumel1103@gmail.com>
2025-04-10 09:38:43 +0200

    Hello

--- tests/cart.test.ts
+++ tests/cart.test.ts
@@ -8,3 +8,12 @@ describe("cart module", () => {
 });
 
 // TODO : ajouter d'autres tests pour le module cart
+
+//test pour l'ajout d'un produit
+it("should add a product to the cart", () => {
+  const cart = [];
+  const product = { id: 1, name: "Product 1", price: 10 };
+  cart.push(product);
+  expect(cart).toEqual([product]);
+});
+
